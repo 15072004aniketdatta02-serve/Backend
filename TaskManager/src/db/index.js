728ffdd9fbc9b2b972617031cb3fe44b5c0cdf2d
ANIKET_DATTA <15.07.2004aniketdatta02@gmail.com>
2025-11-01 20:40:27 +0530

    validators

--- TaskManager/src/db/index.js
+++ TaskManager/src/db/index.js
@@ -1,13 +1,10 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
 
 
-dotenv.config({
-    path:"./.env"
-});
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("✅ MongoDB Connected");
   } catch (err) {
     console.error("❌ MongoDB Connection Error:", err);
