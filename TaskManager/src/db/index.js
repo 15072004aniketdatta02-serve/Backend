0b9f3398465c4b58ac46fb0cedec9b4f5e245f2b
ANIKET_DATTA <15.07.2004aniketdatta02@gmail.com>
2025-11-01 20:20:39 +0530

    project starter code

---
+++ TaskManager/src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+
+dotenv.config({
+    path:"./.env"
+});
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ MongoDB Connection Error:", err);
+    process.exit(1); // Exit if connection fails
+  }
+};
+
+export default connectDB;
