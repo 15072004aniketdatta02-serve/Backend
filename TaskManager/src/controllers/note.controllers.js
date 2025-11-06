74d4ae203f6d386acb4d51ac2de0d806b9cfc5c0
ANIKET_DATTA <15.07.2004aniketdatta02@gmail.com>
2025-11-06 21:49:29 +0530

    controllers

--- TaskManager/src/controllers/note.controllers.js
+++ TaskManager/src/controllers/note.controllers.js
@@ -4,33 +4,41 @@ import { Project } from "../models/Project.models.js";
 import { ApiError } from "../APIStatus/APIError.js";
 import { APIResponse } from "../APIStatus/APIResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+// import { SocketManager } from "../sockets/SocketManager.js";
 const getNotes = asyncHandler(async (req, res) => {
+  // get all notes
+  // get the projectId from the request.params
   const { projectId } = req.params;
+  //try to find the project by projectId
   const project = await Project.findById(projectId);
-
+ //If project not found show appropriate errors 
   if (!project) {
     throw new ApiError(404, "Project not found");
   }
+  //try to get the projectNote 
+  //based on projectId and populate "createdBy" and "username fullName avatar"
   const notes = await ProjectNote.find({
     project: new mongoose.Types.ObjectId(projectId),
   }).populate("createdBy", "username fullName avatar");
-
+  // show notes fetched successfully
   return res
     .status(200)
     .json(new APIResponse(200, notes, "Notes fetched successfully"));
 });
-
+// create notes
 const createNote = asyncHandler(async (req, res) => {
+  // destructure projectId from request parameters 
   const { projectId } = req.params;
+  // destructure content from req.body
+  //{"content":"todo app"}
   const { content } = req.body;
-
+ // find project by Id from db
   const project = await Project.findById(projectId);
-
+// if project not found show appropriate error
   if (!project) {
     throw new ApiError(404, "Project not found");
   }
-
+// create project on the basis of projectId , content  and created by  
   const note = await ProjectNote.create({
     project: new mongoose.Types.ObjectId(projectId),
     content,
@@ -62,11 +70,14 @@ const createNote = asyncHandler(async (req, res) => {
 });
 
 const updateNote = asyncHandler(async (req, res) => {
+  // destructure noteId from req.params
   const { noteId } = req.params;
+  // destructure content from req.body
   const { content } = req.body;
 
   // Find the note first to check if it exists
   const existingNote = await ProjectNote.findById(noteId);
+  //if note exists show appropriate error
   if (!existingNote) {
     throw new ApiError(404, "Note not found");
   }
@@ -97,10 +108,11 @@ const updateNote = asyncHandler(async (req, res) => {
 });
 
 const deleteNote = asyncHandler(async (req, res) => {
+  //destructure the noteId  from the request params
   const { noteId } = req.params;
-
+  // delete on the basis of noteId
   const note = await ProjectNote.findByIdAndDelete(noteId);
-
+// if note is not found show error message
   if (!note) {
     throw new ApiError(404, "Note not found");
   }
@@ -117,24 +129,26 @@ const deleteNote = asyncHandler(async (req, res) => {
       );
     }
   } catch (_) {}
-
+ // success message
   return res
     .status(200)
     .json(new APIResponse(200, note, "Note deleted successfully"));
 });
 
 const getNoteById = asyncHandler(async (req, res) => {
+  // destructure the noteId from req.params
   const { noteId } = req.params;
-
+ // find note from db with the help of noteId , populate with "createdBy" and "username fullName
+ // avatar"
   const note = await ProjectNote.findById(noteId).populate(
     "createdBy",
     "username fullName avatar",
   );
-
+//  if note not found , give correct message 
   if (!note) {
     throw new ApiError(404, "Note not found");
   }
-
+ // for success show appropriate APIresponse
   return res
     .status(200)
     .json(new APIResponse(200, note, "Note fetched successfully"));
--- TaskManager/src/middlewares/auth.middlewares.js
+++ TaskManager/src/middlewares/auth.middlewares.js
@@ -6,26 +6,33 @@ import { ProjectMember } from "../models/Projectmember.model.js";
 import mongoose from "mongoose";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
+  //get the token from cookies or request Header "Authorization"which if present , replace with "Bearer",""
   const token =
     req.cookies?.accessToken ||
     req.header("Authorization")?.replace("Bearer ", "");
-
+//if token is not found , throw API error with message "unauthorized request" 
   if (!token) {
     throw new ApiError(401, "Unauthorized request");
   }
-
+  // try to find the ACCESS_TOKEN in db and then do further work
   try {
+    // try to get the jwt verified decoded token of ACCESS_TOKEN_SECRET
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    // search User on the basis of decoded Access Token and select Password , RefreshToken , emailVerificationToken emailVerificationExpiry
     const user = await User.findById(decodedToken?._id).select(
       "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
     );
+    // if user is not found on the basis of access token throw error
     if (!user) {
       // Client should make a request to /api/v1/users/refresh-token if they have refreshToken present in their cookie
       // Then they will get a new access token which will allow them to refresh the access token without logging out the user
       throw new ApiError(401, "Invalid access token");
     }
+    //if the user's token on the basis of token present in cookies or Authorization header of request is found
+    //store the user in request's user
     req.user = user;
     next();
+    //move to the next part
   } catch (error) {
     // Client should make a request to /api/v1/users/refresh-token if they have refreshToken present in their cookie
     // Then they will get a new access token which will allow them to refresh the access token without logging out the user
@@ -40,15 +47,21 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
  * `NOTE: THIS MIDDLEWARE IS ONLY TO BE USED FOR UNPROTECTED ROUTES IN WHICH THE LOGGED IN USER'S INFORMATION IS NEEDED`
  */
 export const getLoggedInUserOrIgnore = asyncHandler(async (req, res, next) => {
+  //obtain the access token
   const token =
     req.cookies?.accessToken ||
     req.header("Authorization")?.replace("Bearer ", "");
-
+// db operation
   try {
+    //try to obtain the decodedToken from the Access Token
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    // on the basis of decoded access token select the password refreshToken -emailVerificationToken
+    //-emailVerificationExpiry and store that in user  
     const user = await User.findById(decodedToken?._id).select(
       "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
     );
+    //the user rquesting is the same user whose access token secret 
+    // is present in cookies or request header's "Authorization" "Bearer"
     req.user = user;
     next();
   } catch (error) {
@@ -59,24 +72,30 @@ export const getLoggedInUserOrIgnore = asyncHandler(async (req, res, next) => {
 
 export const validateProjectPermission = (roles = []) =>
   asyncHandler(async (req, res, next) => {
+    //obtain the project id from the request parameters
     const { projectId } = req.params;
-
+  // if projectId not found throw error
     if (!projectId) {
       throw new ApiError(400, "Project id is missing");
     }
+    //if the project id is found find the projectMember with the help of project id and user id
     const project = await ProjectMember.findOne({
       project: new mongoose.Types.ObjectId(projectId),
       user: new mongoose.Types.ObjectId(req.user._id),
     });
-
+// if project is not found show appropriate error
     if (!project) {
       throw new ApiError(404, "Project not found");
     }
-
+// store the project role of the user in given role
     const givenRole = project?.role;
-
+// assign the role of the user in the request to thr givenRole 
     req.user.role = givenRole;
-
+// if the roles of the user does not include the givenRole , just throw new Api Error
+//with status code 403 and message that states that the user is not having any permission to perform the action 
+// because the roles that the user is provided with does not include the role of the user trying to 
+// get to the next route and do the further operations 
+// here createNotes in a project    
     if (!roles.includes(givenRole)) {
       throw new ApiError(
         403,
